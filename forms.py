ee9603f3b58f3c34dfff9a5e5b656e886850fd8e
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2023-08-08 21:22:19 -0400

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class MotivateMeForm(forms.Form):
-    pass
-
-
-class ChatBotForm(forms.Form):
-    input_text = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 50}))
